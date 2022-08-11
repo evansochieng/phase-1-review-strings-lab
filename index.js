ec5ad9fbdfe3b2e020965309c9feac754a962401
Evans Ochieng <evansochieng121@gmail.com>
2022-08-11 17:45:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+// Declare current user
+const currentUser = "Evans Ochieng";
+
+// Welcome message
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Excited welcome message
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+// const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Short greeeting
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+//const shortGreeting = `Welcome, ${currentUser[0]}!`;
